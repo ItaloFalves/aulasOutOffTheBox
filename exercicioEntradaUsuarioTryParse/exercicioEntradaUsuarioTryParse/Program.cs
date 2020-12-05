8e0e3a94f95c331d1c6378f5c02d0f5b3deb15d0
italofalves <italofalves@hotmail.com>
2020-12-05 11:45:59 -0300

    Exercicio 2 - Validar entrada de usuário com try.parse

---
+++ exercicioEntradaUsuarioTryParse/exercicioEntradaUsuarioTryParse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicioEntradaUsuarioTryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com um valor numerico: ");
+            var valorDigitadoEhInteiro = int.TryParse(Console.ReadLine(), out int valorDigitado);
+
+            if (valorDigitadoEhInteiro)
+            {
+                Console.WriteLine("O Valor Digitado está certo: {0} \n O valor digitado foi: {1}", valorDigitadoEhInteiro, valorDigitado);
+            }
+            else
+            {
+                Console.WriteLine(" O valor digitado está incorreto.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
